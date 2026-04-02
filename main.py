b947a0549514fab66b74d4bdabbc9b265acb7d39
kayo425 <kaykdvufdtibigydghshiufdhhwjhs@gmail.com>
2026-04-02 23:21:43 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,20 @@
-import fear_and_greed
-index=fear_and_greed.get().value
-index_str=f"{index:.1f}"
-
-!pip install python-dotenv
 import os
-from dotenv import load_dotenv
-load_dotenv("token_ID.env")
-TOKEN=os.getenv("LINE_TOKEN")
-user_id=os.getenv("LINE_user_id")
-
-!pip install line-bot-sdk
-import linebot
-from linebot import (LineBotApi, WebhookHandler)
-from linebot.models import (MessageEvent, TextMessage, TextSendMessage,)
-line_bot_api=LineBotApi(TOKEN)
-
+import fear_and_greed
 from datetime import datetime
+from linebot import LineBotApi
+from linebot.models import TextSendMessage
 
-T_ext=f"{now}\nF&G:{index_str}"
-messages=TextSendMessage(text=T_ext)
+# --- 設定（GitHubの金庫から値を受け取る） ---
+TOKEN = os.getenv("LINE_TOKEN")
+user_id = os.getenv("LINE_user_id")
+
+# --- データの取得と時間の計算 ---
+index = fear_and_greed.get().value
+index_str = f"{index:.1f}"
+now = datetime.now().strftime("%Y/%m/%d(%a) %H:%M")
+
+# --- LINE送信の実行 ---
+T_ext = f"{now}\nF&G:{index_str}"
+line_bot_api = LineBotApi(TOKEN)
+messages = TextSendMessage(text=T_ext)
 line_bot_api.push_message(user_id, messages=messages)
